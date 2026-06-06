6342c7220afc360d1f95699e3bff861ad8a16fe8
FUJI <jazzmaster204@gmail.com>
2026-06-06 19:59:35 +0300

    add request params

--- src/main/java/com/tommy/mvc/MyController.java
+++ src/main/java/com/tommy/mvc/MyController.java
@@ -3,6 +3,7 @@ package com.tommy.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,13 +21,10 @@ public class MyController {
     }
 
     @RequestMapping("/showDetails")
-    public String showDetails (HttpServletRequest request, Model model) {
-        String empName = request.getParameter("employeeName");
+    public String showDetails (@RequestParam("employeeName")
+                               String empName, Model model) {
         empName = "Mr " + empName;
         model.addAttribute("nameAttribute", empName);
-
-
-
         return "show-details";
     }
 
